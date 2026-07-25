ea03b884b81a235388abd5b49e2b9c1cb7bd11ee
Paul Parlett <frialey63@googlemail.com>
2026-07-25 22:16:02 +0100

    Fix recursive argument in list_files_glob function

--- cat_project_files.py
+++ cat_project_files.py
@@ -10,7 +10,7 @@ PATTERNS = [ './**/*.MF', './**/*.java', './**/*.properties', './**/*.xml', './*
 outputFile = ''
 
 def list_files_glob(pattern = './**/*.*', recursive = True):
-    files = glob.glob(pattern, recursive)
+    files = glob.glob(pattern, recursive = True)
 
     for file in files:
         process_file(file)
